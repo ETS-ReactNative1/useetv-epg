ba26305cc0825a22000d92dd6e2244ac51b4dbbd
badrus123 <badrussholehaxel@gmail.com>
2021-08-03 04:11:17 +0700

    done for send wa from web

--- src/pages/message/SendMessage.js
+++ src/pages/message/SendMessage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Grid,
   Paper,
@@ -6,12 +6,21 @@ import {
   makeStyles,
   Typography,
   Button,
+  Chip,
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
-
+import "./style.css";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 const useStyles = makeStyles((theme) => ({
   paper: {
     padding: 20,
+  },
+  paperPreview: {
+    padding: 20,
+    backgroundImage: `url(
+      "https://1.bp.blogspot.com/-vnihMx5Kvq8/XOAwcL02yyI/AAAAAAAAZGA/KXI7fS-BhTIX-2Q1vf5YkfvPr_YrjCNFwCLcBGAs/s1600/1.jpg")`,
+    height: "100%",
   },
   input: {
     marginTop: 10,
@@ -32,23 +41,92 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#5cb85c ",
     color: "white",
   },
+  img: {
+    width: "100%",
+    height: 300,
+    objectFit: "cover",
+  },
 }));
 export default function SendMessage() {
   var classes = useStyles();
-
+  let history = useHistory();
+  const [value, setValue] = useState({
+    category: "BRONZE",
+    image: null,
+    message: "",
+    date: new Date(),
+    time: new Date(),
+  });
+  const handleChange = (name) => (e) => {
+    setValue({ ...value, [name]: e.target.value });
+  };
+  const handleFile = (e) => {
+    const tempFile = Array.from(e.target.files);
+    const formData = new FormData();
+    formData.append("image", tempFile[0]);
+    axios
+      .post(`http://157.230.47.142:9091/file/upload-image`, formData)
+      .then((res) => {
+        setValue({ ...value, image: res.data });
+      });
+  };
+  const handleSubmit = () => {
+    const noImage = {
+      phone: "0895346604342",
+      message: value.message,
+      image: value.image,
+      caption: "string",
+      date: "string",
+      time: "string",
+      secret: true,
+      priority: true,
+    };
+    const withImage = {
+      customerCategory: "BRONZE",
+      caption: value.message,
+      image: value.image,
+    };
+    axios
+      .post(
+        `http://157.230.47.142:9091/whatsapp/wablas/send-message-${
+          value.image !== null ? "with" : "without"
+        }-image`,
+        value.image !== null ? withImage : noImage,
+      )
+      .then(() => {
+        history.push("/app/message");
+      });
+  };
   return (
-    <Paper className={classes.paper}>
-      <Grid container={3} spacing={3}>
-        <Grid item xs={12}>
+    <Grid container spacing={3}>
+      <Grid item lg={8} md={8} sm={12} xl={8}>
+        <Paper className={classes.paper}>
           <Typography variant="body1">Category User</Typography>
-          <TextField variant="outlined" className={classes.input} fullWidth />
-          <Typography variant="body1">Image</Typography>
           <TextField
             variant="outlined"
             className={classes.input}
             fullWidth
-            type="file"
+            onChange={handleChange("category")}
+            value={value.category}
+            disabled
           />
+          <Typography variant="body1">Image</Typography>
+          {value.image !== null ? (
+            <Chip
+              label={value.image}
+              onDelete={() => setValue({ ...value, image: null })}
+              className={classes.chip}
+            />
+          ) : (
+            <TextField
+              variant="outlined"
+              className={classes.input}
+              fullWidth
+              type="file"
+              onChange={handleFile}
+            />
+          )}
+
           <Typography variant="body1">Body message</Typography>
           <TextField
             variant="outlined"
@@ -56,6 +134,8 @@ export default function SendMessage() {
             multiline
             rows="10"
             fullWidth
+            onChange={handleChange("message")}
+            value={value.message}
           />
           <Typography variant="body1">Date</Typography>
           <TextField
@@ -63,6 +143,8 @@ export default function SendMessage() {
             className={classes.input}
             fullWidth
             type="date"
+            onChange={handleChange("date")}
+            value={value.date}
           />
           <Typography variant="body1">Time</Typography>
           <TextField
@@ -70,6 +152,8 @@ export default function SendMessage() {
             className={classes.input}
             fullWidth
             type="time"
+            onChange={handleChange("time")}
+            value={value.time}
           />
           <div className={classes.divFlex}>
             <Link to="/app/message" className={classes.cancel}>
@@ -79,12 +163,27 @@ export default function SendMessage() {
               variant="contained"
               className={classes.button}
               size="medium"
+              onClick={handleSubmit}
             >
               Submit
             </Button>
           </div>
-        </Grid>
+        </Paper>
       </Grid>
-    </Paper>
+      <Grid item lg={4} md={4} sm={12} xl={4}>
+        <Paper className={classes.paperPreview}>
+          {value.message.length > 0 || value.image !== null ? (
+            <div className="box sb1">
+              {value.image !== null ? (
+                <img src={value.image} className={classes.img} />
+              ) : null}
+              <Typography variant="body2" component="p">
+                {value.message}
+              </Typography>
+            </div>
+          ) : null}
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
